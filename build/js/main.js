77b98e49ac12aa2bc678b8290c10b2d8b23ac2b0
Kirillovich <kir3316@yandex.ru>
2023-01-19 09:15:20 +0500

    test

--- build/js/main.js
+++ build/js/main.js
@@ -1,36 +1,61 @@
 window.onload = function () {
-   history.pushState('', document.title, window.location.pathname);
+   // history.pushState('', document.title, window.location.pathname);
+   // console.log(location.hash);
+
+   const widthWindow = document.documentElement.clientWidth;
+
+   const idChats = document.querySelector('#chats.chat-sidebar__content');
+   const idContacts = document.querySelector('#contacts.chat-sidebar__content');
+   const idProfile = document.querySelector('#profile.chat-sidebar__content');
+   const currentChat = document.querySelector('.chat-content');
+   const chatSidebar = document.querySelector('.chat-sidebar');
+   const chat = document.querySelector('.chat');
+
+   if (location.hash === '#contacts') {
+      idContacts.style.display = 'flex';
+   } else if (location.hash === '#chats') {
+      idChats.style.display = 'flex';
+      idContacts.style.display = 'none';
+   } else if (location.hash === '#profile') {
+      idProfile.style.display = 'flex';
+      idContacts.style.display = 'none';
+   }
+
+   if (location.hash === '#current-chat') {
+      currentChat.style.display = 'flex';
+      idContacts.style.display = 'none';
+
+      currentChat.style.height = '0';
+      chatSidebar.style.flexBasis = '0';
+      chat.style.flexDirection = 'column-reverse';
+   } else {
+      chatSidebar.style.flexBasis = '100%';
+      chat.style.flexDirection = 'initial';
+      currentChat.style.height = 'initial';
+   }
 
    window.addEventListener('hashchange', hashchange);
 
    function hashchange() {
       const hash = location.hash;
-      const widthWindow = document.documentElement.clientWidth;
-
-      const idChats = document.querySelector('#chats.chat-sidebar__content');
-      const idContacts = document.querySelector('#contacts.chat-sidebar__content');
-      const idProfile = document.querySelector('#profile.chat-sidebar__content');
-      const currentChat = document.querySelector('.chat-content');
-      const chatSidebar = document.querySelector('.chat-sidebar');
-      const chat = document.querySelector('.chat');
 
       if (hash === '#contacts') {
          idContacts.style.display = 'flex';
          idChats.style.display = 'none';
          idProfile.style.display = 'none';
-         widthWindow < 1000 ? currentChat.style.display = 'none' : null;
+         widthWindow < 1000 ? (currentChat.style.display = 'none') : null;
       } else if (hash === '#chats') {
          idChats.style.display = 'flex';
          idContacts.style.display = 'none';
          idProfile.style.display = 'none';
-         widthWindow < 1000 ? currentChat.style.display = 'none' : null;
+         widthWindow < 1000 ? (currentChat.style.display = 'none') : null;
       } else if (hash === '#profile') {
          idProfile.style.display = 'flex';
          idChats.style.display = 'none';
          idContacts.style.display = 'none';
-         widthWindow < 1000 ? currentChat.style.display = 'none' : null;
-      } 
-      
+         widthWindow < 1000 ? (currentChat.style.display = 'none') : null;
+      }
+
       if (hash === '#current-chat') {
          currentChat.style.display = 'flex';
          currentChat.style.height = '0';
@@ -45,5 +70,7 @@ window.onload = function () {
          chat.style.flexDirection = 'initial';
          currentChat.style.height = 'initial';
       }
+
+      console.log(hash);
    }
 };

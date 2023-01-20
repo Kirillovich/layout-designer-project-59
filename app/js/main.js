28947ec1215ea6b6029451ab449851f86844989c
Kirillovich <kir3316@yandex.ru>
2023-01-20 14:26:02 +0500

    test!

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,43 @@
 window.onload = function () {
-   const idAll = document.querySelectorAll('.chat-sidebar__content');
    const idContacts = document.querySelector('#contacts.chat-sidebar__content');
    const idChats = document.querySelector('#chats.chat-sidebar__content');
-   // const idProfile = document.querySelector('#profile.chat-sidebar__content');
+   const idProfile = document.querySelector('#profile.chat-sidebar__content');
+
+   const linkContacts = document.querySelector('a[href="#contacts"]');
+   const linkChats = document.querySelector('a[href="#chats"]');
+   const linkProfile = document.querySelector('a[href="#profile"]');
+
+   const idAll = document.querySelectorAll('.chat-sidebar__content');
    const chatContent = document.querySelector('.chat-content');
    const chatSidebar = document.querySelector('.chat-sidebar');
    const chat = document.querySelector('.chat');
 
+   linkContacts.addEventListener('click', function() {
+      idContacts.style.display = 'flex';
+      idChats.style.display = 'none';
+      idProfile.style.display = 'none';
+   });
+
+   linkChats.addEventListener('click', function() {
+      idContacts.style.display = 'none';
+      idChats.style.display = 'flex';
+      idProfile.style.display = 'none';
+   });
+
+   linkProfile.addEventListener('click', function() {
+      idContacts.style.display = 'none';
+      idChats.style.display = 'none';
+      idProfile.style.display = 'flex';
+   });
+
    const makeIdVisible = () => {
-      for (id of idAll) {
-         if (`#${id.getAttribute('id')}` === location.hash) {
-            id.style.display = 'flex';
-         } else {
-            id.style.display = 'none';
-         }
-      }
+      // for (id of idAll) {
+      //    if (`#${id.getAttribute('id')}` === location.hash) {
+      //       id.style.display = 'flex';
+      //    } else {
+      //       id.style.display = 'none';
+      //    }
+      // }
 
       if (document.documentElement.clientWidth < 1000) {
          if (location.hash === '#current-chat') {
@@ -44,13 +67,13 @@ window.onload = function () {
 
    makeIdVisible();
 
-   if (location.pathname === '/' && location.hash === '') {
-      idContacts.style.display = 'flex';
-   }
+   // if (location.pathname === '/' && location.hash === '') {
+   //    idContacts.style.display = 'flex';
+   // }
 
-   if (location.pathname === '/chat.html' && location.hash === '') {
-      idChats.style.display = 'flex';
-   }
+   // if (location.pathname === '/chat.html' && location.hash === '') {
+   //    idChats.style.display = 'flex';
+   // }
 
    window.addEventListener('hashchange', hashchange);
 

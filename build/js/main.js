0551ec6806ea7d690085a0ecbe7614b7a3699951
Kirillovich <kir3316@yandex.ru>
2023-01-28 23:55:12 +0500

    pet

--- build/js/main.js
+++ build/js/main.js
@@ -1,18 +1,27 @@
 window.onload = function () {
    const contacts = document.querySelector('#contacts');
+   const chats = document.querySelector('#chats');
+   const currentChat = document.querySelector('#current-chat');
    const linksMenu = document.querySelectorAll('.nav-link');
    const allBlocks = document.querySelectorAll('.tab-pane');
-   // const chats = document.querySelector('#chats');
    // const profile = document.querySelector('#profile');
 
    const hash = location.hash;
+   const pathname = location.pathname;
    const contentBlock = hash === '' ? '' : document.querySelector(hash);
 
-   if (hash) {
+   if (hash && pathname === '/') {
       contentBlock.style.order = '-1';
       contentBlock.style.display = 'flex';
    } else {
       contacts.style.display = 'flex';
+   }
+
+   if (hash && pathname !== '/') {
+      contentBlock.style.order = '-1';
+      contentBlock.style.display = 'flex';
+   } else {
+      chats.style.display = 'flex';
    }
 
    for (elem of linksMenu) {
